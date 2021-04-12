4718987552b3950f4bd8be63910b51d31f4d3375
ww-diego-raffaeli <diego.raffaeli@wirewheel.io>
2021-04-12 11:29:47 -0300

    transformation service refactor

--- service/transformation-service.js
+++ service/transformation-service.js
@@ -1,43 +1,18 @@
-// transformation-service.js
+// entity-service.js
 
 const entityService = require('./entity-service')
-const rqModel = require('../model/transformation-rq')
-const Type = rqModel.Type
-
 
 async function transform(transformationRq) {
-    const results = []
 
-    // users
-    const userEntity = transformationRq.entities.find(findEntity(Type.USER))
-    if (userEntity != undefined) {
-        console.log('Processing users sources' + JSON.stringify(userEntity.targets))
-        const userResult = await entityService.fetch(userEntity.targets)
-        console.log('Users obtained: ' + JSON.stringify(userResult))
-        results.push({entity: userEntity, result: userResult})
-    } else {
-        console.log("User entity not present")
-    }
+    const promises = transformationRq.entities.map(async entity => {
+        const type = entity.type
+        console.log('Processing entity sources' + JSON.stringify(entity.targets))
+        const result = await entityService.fetch(entity.targets)
+        console.log('Items obtained: ' + JSON.stringify(result))
+        return {entity: entity, result: result}
+    })
 
-    // attachments
-    // TODO
-
-    // assessments
-    const assessmentEntity = transformationRq.entities.find(findEntity(Type.ASSESSMENT))
-    if (assessmentEntity != undefined) {
-        console.log('Processing assessment sources' + JSON.stringify(assessmentEntity.targets))
-        const assessmentResult = await entityService.fetch(assessmentEntity.targets)
-        console.log('Assessment obtained: ' + JSON.stringify(assessmentResult))
-        results.push({entity: assessmentEntity, result: assessmentResult})
-    } else {
-        console.log("Assessment entity not present")
-    }
-
-    return results
-}
-
-function findEntity(type) {
-    return entity => entity.type === type;
+    return await Promise.all(promises)
 }
 
 module.exports = {transform}
